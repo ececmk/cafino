82c8d5c0dfa870d023b6a97a37f2253c67fa37b1
Ece Çakmak <86318154+ececmk@users.noreply.github.com>
2023-08-18 13:50:40 +0200

    reviewsData updated

--- src/Components/HomeComponents/Reviews/reviewsData.js
+++ src/Components/HomeComponents/Reviews/reviewsData.js
@@ -2,38 +2,50 @@ const reviews = [
     {
       id: 1,
       name: 'Figen A.',
-      text:
+      text_en:
         'I liked it very much. It is a clean place, the staff is very friendly and caring.',
+      text_de:
+        'Es hat mir sehr gut gefallen. Es ist ein sauberer Ort, das Personal ist sehr freundlich und fürsorglich.',
     },
     {
       id: 2,
       name: 'Alisha C.',
-      text:
+      text_en:
         'Highly recommended!! Very nice cafe and friendly staff.',
+      text_de:
+        'Sehr zu empfehlen!! Sehr schönes Cafe und freundliches Personal.',
     },
     {
       id: 3,
       name: 'Alex T.',
-      text:
+      text_en:
         'Lovely place in Trudering! Very friendly stuff, calm & relaxing atmosphere, tasty food, all of that give you a great homelike feeling.',
+      text_de: 
+        'Schöner Ort in Trudering! Sehr freundliches Personal, ruhige und entspannte Atmosphäre, leckeres Essen, das alles gibt einem ein tolles Gefühl von Zuhause.'
     },
     {
         id: 4,
         name: 'Wilhelm R.',
-        text:
+        text_en:
           'The owner is vegan, so almost everything is also available in a vegan version. The tarte flambée was delicious. Very friendly and cozy café.',
+        text_de: 
+          'Die Besitzerin ist Veganerin, daher ist fast alles auch in einer veganen Version erhältlich. Der Flammkuchen war köstlich. Sehr freundliches und gemütliches Café.'
       },
       {
         id: 5,
         name: 'Grace Gamze O.',
-        text:
+        text_en:
           'Always nice to have a short break at the Cafino. The food is delicious, the cake a dream, the service attentive. We always feel welcome.',
+        text_de:
+          'Es ist immer schön, eine kurze Pause im Cafino einzulegen. Das Essen ist köstlich, der Kuchen ein Traum, der Service zuvorkommend. Wir fühlen uns immer willkommen.' 
       },
       {
         id: 6,
         name: 'Susana C.',
-        text:
+        text_en:
           'Very special place, mix of lovely deco shop and cafe, breakfast, healthy lunch and dinner. Highly recommend!',
+        text_de:
+          'Ein ganz besonderer Ort, eine Mischung aus schönem Dekoladen und Café, Frühstück, gesundes Mittag- und Abendessen. Sehr empfehlenswert!'
       },
   ];
   
