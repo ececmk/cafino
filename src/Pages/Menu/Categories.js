226f6dbda9007fea2df674e0285c8852532ac361
Ece Çakmak <86318154+ececmk@users.noreply.github.com>
2023-08-22 22:10:11 +0200

    categories translation problem solved

---
+++ src/Pages/Menu/Categories.js
@@ -0,0 +1,10 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+    wine: { key: 'wine' },
+    aperatif: { key: 'aperatif' },
+    flammkuchen: { key: 'flammkuchen' },
+    bagel: { key: 'bagel' },
+    salad: { key: 'salad' },
+    hotBeverages: { key: 'hot-beverages' },
+    coldBeverages: { key: 'cold-beverages' },
+  }
